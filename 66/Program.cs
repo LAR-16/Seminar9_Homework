d4a8b4e96b1b83e67720e5eec1e6585102a2c5cd
Alexandra <lebedeva1618@yandex.ru>
2022-12-24 23:05:24 +0300

    66. Сумма элементов от М до Н

---
+++ 66/Program.cs
@@ -0,0 +1,14 @@
+﻿int sum(int a, int b)
+{
+    if (a == b)
+        return a;
+    return sum(a + 1, b) + a;
+}
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(M, N));
