d6842d7bb2f892346bca932b0263105e4a462657
Alexandra <lebedeva1618@yandex.ru>
2022-12-24 23:08:44 +0300

    68. Аккерман

---
+++ 68/Program.cs
@@ -0,0 +1,26 @@
+﻿int Akk(int a, int b)
+{
+    if (a == 0)
+        return b + 1;
+    else if (b == 0)
+        return Akk(a - 1, 1);
+    return Akk(a - 1, Akk(a, b - 1));
+}
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+while (m < 0)
+{
+    Console.Write("Введите 1-ое число: ");
+    m = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 0)
+{
+    Console.Write("Введите 2-ое число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(Akk(m, n));
